70fc5c557b686c5946b9c1a94299d28adf78bc69
Siddharth <moonmars2au@gmail.com>
2024-11-04 17:20:03 +0530

    shgshgds

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import Event from './pages/Event/Event';
 import Payments from './pages/Payments/Payments';
 import Login from './pages/Login/LoginPage';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Homepage />,
--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -47,46 +47,50 @@ function Payments() {
 
 function PaymentCard({ payment }) {
   return (
-    <div className="max-w-sm p-6 mx-auto mt-10 bg-gray-100 rounded-lg shadow-neumorphism">
-      <h2 className="text-xl font-semibold text-gray-800 mb-2">
-        Payment Details
-      </h2>
+    <div>
+      <div className="max-w-sm p-6 mx-auto mt-10 bg-gray-100 rounded-lg shadow-neumorphism">
+        <h2 className="text-xl font-semibold text-gray-800 mb-2">
+          Payment Details
+        </h2>
 
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold text-gray-700">Email</h3>
-        <p className="text-gray-600">{payment.email}</p>
-      </div>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold text-gray-700">Email</h3>
+          <p className="text-gray-600">{payment.email}</p>
+        </div>
 
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold text-gray-700">
-          Registration IDs
-        </h3>
-        <ul className="list-disc list-inside text-gray-600">
-          {payment.registrationIds.map((id, index) => (
-            <li key={index}>{id}</li>
-          ))}
-        </ul>
-      </div>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold text-gray-700">
+            Registration IDs
+          </h3>
+          <ul className="list-disc list-inside text-gray-600">
+            {payment.registrationIds.map((id, index) => (
+              <li key={index}>{id}</li>
+            ))}
+          </ul>
+        </div>
 
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold text-gray-700">Order ID</h3>
-        <p className="text-gray-600">{payment.paymentInfo.order_id}</p>
-      </div>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold text-gray-700">Order ID</h3>
+          <p className="text-gray-600">{payment.paymentInfo.order_id}</p>
+        </div>
 
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold text-gray-700">Payment ID</h3>
-        <p className="text-gray-600">{payment.paymentInfo.payment_id}</p>
-      </div>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold text-gray-700">Payment ID</h3>
+          <p className="text-gray-600">{payment.paymentInfo.payment_id}</p>
+        </div>
 
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold text-gray-700">Payment Status</h3>
-        <p
-          className={`text-lg font-bold ${
-            payment.paymentSuccess ? 'text-green-600' : 'text-red-600'
-          }`}
-        >
-          {payment.paymentSuccess ? 'Success' : 'Pending'}
-        </p>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold text-gray-700">
+            Payment Status
+          </h3>
+          <p
+            className={`text-lg font-bold ${
+              payment.paymentSuccess ? 'text-green-600' : 'text-red-600'
+            }`}
+          >
+            {payment.paymentSuccess ? 'Success' : 'Pending'}
+          </p>
+        </div>
       </div>
     </div>
   );
